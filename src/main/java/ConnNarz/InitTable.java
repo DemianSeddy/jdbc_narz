2175f623523dd01435deaa98a014b8fc95627107
Pridvorov <pridvorov@gmai.com>
2020-08-03 13:09:43 +0700

    InitTable

--- src/main/java/ConnNarz/InitTable.java
+++ src/main/java/ConnNarz/InitTable.java
@@ -1,39 +1,46 @@
 package ConnNarz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 public class InitTable {
 
     //driver={SQL Server};server=HPDL380;uid=reader;pwd=1234qsw;database=Документация;
 
-      String connectionUrl =
-            "jdbc:sqlserver://HPDL380:1433;"
-                    + "database=Документация;"
-                    + "user=pridvorov@narp;"
-                    + "password=Pridvorov1509@;"
-                    + "encrypt=true;"
-                    + "trustServerCertificate=false;"
-                    + "loginTimeout=30;";
+    private final String connectionUrl = "jdbc:jtds:sqlserver://HPDL380;database=Документация;domain=NARP;loginTimeout=300;";
+    private final String user = "pridvorov";
+    private final String password = "Pridv0r0v0915@";
 
-    public InitTable() throws SQLException {
+    private final String sqlStringOstatkiall = "SELECT * FROM OSTATKIFORTOIRFROMNARZ";
+    private final String sqlStringOstatkiForOne = "SELECT * FROM OSTATKIFORTOIRFROMNARZ where nomen ?";
 
+    public InitTable() {
         try {
-              Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-              System.out.print("Connecting to SQL Server ... ");
-              try (Connection connection = DriverManager.getConnection(connectionUrl)) {
-                  System.out.println("Done.");
-              }
-                  catch (SQLException ex)
-                  {
-                      System.out.println(ex);
-                  }
-        } catch (ClassNotFoundException e) {
-                   System.out.println("Unable to load class.");
-                   e.printStackTrace();
+            Connection conn = getNewConnection();
+            try {
+                Statement statement = this.getNewConnection().createStatement();
+                try {
+                    ResultSet executeQuery = statement.executeQuery(sqlStringOstatkiall);
+                    return
+                            ;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    Connection getNewConnection() throws SQLException
+    {
+        return DriverManager.getConnection(connectionUrl, this.user, this.password);
     }
+
+
+}
+
+
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,38 @@
 import ConnNarz.InitTable;
 import FromCsv.LoadFromCsv;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         //new LoadFromCsv();
         InitTable conn = new InitTable();
 
-          }
+   /**     if (conn.shouldGetJdbcConnection() != null) {
+
+            System.out.println(conn.shouldGetJdbcConnection().getSchema().toString());
+            Statement stmt = conn.shouldGetJdbcStatement(conn.shouldGetJdbcConnection());
+            System.out.println(stmt.getConnection());
+
+            if (stmt != null) {
+                ResultSet executeQuery = stmt.executeQuery("SELECT * FROM OSTATKIFORTOIRFROMNARZ");
+                // Обход результатов выборки
+                while (executeQuery.next()) {
+                    System.out.println(executeQuery.getString("Category"));
+                }
+                // Закрываем соединение
+                executeQuery.close();
+                stmt.close();
+            }
+
+        }
+ **/
+
+    }
+
+
 }
